5982ed1c9b19d4e62949f5d11c661b20ea9ef622
nick pai <>
2018-12-10 09:59:42 -0500

    serve -s build

---
+++ build/precache-manifest.45d92f194b6bbda5059e12fe480658b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "547eeb6744ab5992613f",
+    "url": "/static/css/main.97598d5e.chunk.css"
+  },
+  {
+    "revision": "547eeb6744ab5992613f",
+    "url": "/static/js/main.547eeb67.chunk.js"
+  },
+  {
+    "revision": "d2e2f67455d153e4e88f",
+    "url": "/static/js/1.d2e2f674.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36e4b67dd63c19d143b1af311ea84bce",
+    "url": "/index.html"
+  }
+];
