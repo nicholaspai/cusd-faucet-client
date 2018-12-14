b5e880517164f5e727ff1657850b999e3ce09bd8
nick pai <>
2018-12-13 22:09:30 -0500

    Revert "registering new users on create account works"
    This reverts commit e74ddf835ba09d9fb3a4d3166610e750ca81b4a2.

--- src/db_services/saveUser.js
+++ src/db_services/saveUser.js
@@ -4,35 +4,18 @@ import config from '../config'
 const SERVER = config.server_url
 const USERS_ENDPOINT = SERVER+"api/accounts/users"
 
-const saveUser = async (new_username, new_password) => {
+const saveUser = async (post_data) => {
     try {
-        let post_data = {
-            user: new_username,
-            password: new_password, 
-        }
-        let response = await axios.post(
+        // let db_get_response = await axios.get(
+        //     USERS_ENDPOINT
+        // )
+        let db_post_response = await axios.post(
             USERS_ENDPOINT,
             post_data
-        );
-        // new user was created
-        if (response.data) {
-            let key = response.data.key
-            let value = response.data.value
-            console.log('key: ', key)
-            console.log('value: ', value)
-
-            return {
-                key, value
-            }
-        } else {
-            // error or user already exists
-            console.log('invalid account or user already exists')
-            return false
-        } 
+          );
+        return db_post_response; 
     } catch(err) {
         console.log('unable to connect users accounts database: ', USERS_ENDPOINT)
-        // console.log(err)
-        return  false
     }
     
 }
