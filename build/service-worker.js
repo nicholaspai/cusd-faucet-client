cede597e361779ea368ae87f1553e600bac58760
Joel Battsek <jbattsek@Joels-MBP.fios-router.home>
2019-02-01 15:08:01 -0500

    fix balance comparitor

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e7d5e45ed76b7185dc57fecd04752eb3.js"
+  "/precache-manifest.6770548294c3180958280616b10eb943.js"
 );
 
 workbox.clientsClaim();
